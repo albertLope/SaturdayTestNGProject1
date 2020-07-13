424c31941a30b4cd9ab3efb11f8a88a45d3789e7
mhmmtkkl <makoklu32@gmail.com>
2020-07-13 15:04:38 -0400

    Project

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -1,10 +1,6 @@
 package Tests;
 
-import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class Test1 extends BaseDriver{
+public class Test1 {
 
     /*
 
@@ -25,42 +21,4 @@ public class Test1 extends BaseDriver{
 
      */
 
-    @Test
-    public void test1(){
-
-        driver.findElement(By.id("nav-secondary-contacts")).click();
-
-        driver.findElement(By.xpath("//span[text()='Create contact']")).click();
-
-        String email="halitt@gmail.com";
-        driver.findElement(By.xpath("//input[@data-selenium-test='property-input-email']")).sendKeys(email);
-
-        String fname="steven";
-        driver.findElement(By.xpath("//input[@data-selenium-test='property-input-firstname']")).sendKeys(fname);
-
-        String lname="gerrard";
-        driver.findElement(By.xpath("//input[@data-selenium-test='property-input-lastname']")).sendKeys(lname);
-
-        driver.findElement(By.xpath("//button[@data-selenium-test='base-dialog-confirm-btn']")).click();
-
-        String fNameVal =  driver.findElement(By.xpath("//input[@data-selenium-test='property-input-firstname']")).getAttribute("value");
-
-        String emailVal =   driver.findElement(By.xpath("//div[@data-selenium-test='property-input-email']")).getText();
-
-        Assert.assertEquals(fNameVal ,fname );
-        Assert.assertEquals(emailVal ,email );
-
-        driver.findElement(By.xpath("//span[text()='Actions']")).click();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.findElement(By.xpath("//button[@data-selenium-test='profile-settings-profileSettings.delete']")).click();
-
-        driver.findElement(By.xpath("//button[@data-selenium-test='delete-dialog-confirm-button']")).click();
-
-
-    }
 }
